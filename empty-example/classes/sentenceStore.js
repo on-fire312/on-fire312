761f2fafce2003905adadc9b2cfdc5f25b307251
on-fire312 <90339111+on-fire312@users.noreply.github.com>
2022-11-25 20:03:22 +0000

    Delete empty-example/classes directory

--- empty-example/classes/sentenceStore.js
+++
@@ -1,8 +0,0 @@
-//Written by Oscar Hitchcock-Smith
-//This is the dataStore class this is used to store the settings from local storage to easily save/load it
-class dataStore {
-    constructor(nSentence, nFilter) {
-        this.filter = nFilter
-        this.sentence = nSentence
-    }
-};
